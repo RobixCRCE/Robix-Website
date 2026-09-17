778766c522e7909302a0f78f452d08e75c2f6615
YK1218 <fs22ai007yashkdhasal@gmail.com>
2026-09-18 02:34:02 +0530

    this too

--- src/main.js
+++ src/main.js
@@ -2,16 +2,28 @@ import "./styles/global.css";
 import "./styles/preloader.css";
 import "./styles/hero.css";
 
+
 import {
   initPreloader
 } from "./animations/preloader.js";
 
 
+import {
+  initHero
+} from "./animations/hero.js";
+
+
+
 document.addEventListener(
+
   "DOMContentLoaded",
+
   () => {
+
+    initHero();
 
     initPreloader();
 
   }
+
 );
